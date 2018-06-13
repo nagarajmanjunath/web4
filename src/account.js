fd7cbd8da51ca429a7942104619210ba06a1cf90
nagarajmanjunath <38091008+nagarajmanjunath@users.noreply.github.com>
2018-06-13 16:20:26 +0800

    Create account.js

---
+++ src/account.js
@@ -0,0 +1,59 @@
+ var server = require('../src/server');
+ var errormsg = '';
+ var parameter = [];
+ let BURROW = "burrow"
+ var ser = new server();
+module.exports = class Account {
+  
+  constructor(server,parameter){
+       this.server = server;
+       this.parameter = parameter;
+    }
+   
+    //** get all accounts details with balance */
+ getAccounts = function (server) {  
+    try{
+      if(server !='' && server != "undefined"){
+        this.server = server;
+        this.parameter = {"filters": []};
+        ser.serverPost("burrow.getAccounts",this.server,this.parameter,function(error,data){
+            console.log("getAccounts",data);
+       });
+      }
+      else{        
+          throw "getAccount error url or method is undefined";
+      }
+    }
+    catch(ex){
+      throw ex;
+    }
+    
+  }
+
+   //** get storage realted address */
+  getStorage = function (server,address) { 
+    try{
+      this.server = server;
+      this.parameter = {"address":address};
+      server.prototype.serverPost("burrow.getStorage",this.server,this.parameter);
+    }
+    catch (ex){
+      throw ex;
+
+    }      
+   
+  }
+
+  //** get storageAt realted address */
+  getStorageAt = function (server,address, key) {
+   
+     try {
+      this.server = serverURl;
+      this.parameter = {"address":address, "key":key};
+      server.prototype.serverPost("burrow.getStorageAt",this.server,this.parameter);
+     }catch (ex){
+
+      throw ex;
+     }    
+  }
+}
